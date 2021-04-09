dc675ad211ecc2304ecc39dff868c221c5ea1d67
Mihail Georgiev <misho_0501@abv.bg>
2021-04-09 18:33:13 +0300

    Print all estates in table view

--- kursova/RealEstate.cpp
+++ kursova/RealEstate.cpp
@@ -13,6 +13,20 @@ list<Estate*> RealEstate::getEstates() {
 }
 
 void RealEstate::printEstates() {
+
+	cout << setw(126) << setfill('-') << "" << endl;
+	cout << setw(10) << setfill(' ') << left << "|Type"
+		<< setw(25) << setfill(' ') << left << "|Address"
+		<< setw(15) << setfill(' ') << left << "|Owner"
+		<< setw(10) << setfill(' ') << left << "|Price"
+		<< setw(10) << setfill(' ') << left << "|Size"
+		<< setw(10) << setfill(' ') << left << "|Rooms"
+		<< setw(10) << setfill(' ') << left << "|Floor"
+		<< setw(15) << setfill(' ') << left << "|Yard size"
+		<< setw(20) << setfill(' ') << left << "|Comunications"
+		<< "|" << endl;
+	cout << setw(126) << setfill('-') << "" << endl;
+
 	for (auto estate : estates)
 	{
 		estate->printEstate(cout);
