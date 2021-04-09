bc282274b65fb6fe7b10fd40c91a81a0f7d825a9
Mihail Georgiev <misho_0501@abv.bg>
2021-04-09 18:04:47 +0300

    Fix setting the percent for the broker and returnin original price afterwards

--- kursova/DishonestBroker.cpp
+++ kursova/DishonestBroker.cpp
@@ -10,13 +10,35 @@ DishonestBroker::DishonestBroker(const string& Name, const double& Percent, Real
 	VIPBroker(Name, Percent, Estates) {}
 
 void DishonestBroker::printFoundEstates(list<Estate*> found, list<Estate*>* vip) {
+	cout << setw(126) << setfill('-') << "" << endl;
+	cout << setw(10) << setfill(' ') << left << "|Type"
+		<< setw(25) << setfill(' ') << left << "|Address"
+		<< setw(15) << setfill(' ') << left << "|Owner"
+		<< setw(10) << setfill(' ') << left << "|Price"
+		<< setw(10) << setfill(' ') << left << "|Size"
+		<< setw(10) << setfill(' ') << left << "|Rooms"
+		<< setw(10) << setfill(' ') << left << "|Floor"
+		<< setw(15) << setfill(' ') << left << "|Yard size"
+		<< setw(20) << setfill(' ') << left << "|Comunications"
+		<< "|" << endl;
+	cout << setw(126) << setfill('-') << "" << endl;
+	
 	for (auto e : *vip) {
+		int prevPrice = e->getPrice();
+		int prevSize = e->getSize();
+
+		e->setPrice(prevPrice + prevPrice * percent);
+
 		e->setPrice(e->getPrice() * ((double)rand() / (RAND_MAX)));
 		e->setSize(e->getSize() * ((double)rand() / (RAND_MAX)));
-		cout << e;
+
+		cout << *e;
+
+		e->setPrice(prevPrice);
+		e->setSize(prevSize);
 	}
 
 	for (auto e : found) {
-		cout << e;
+		cout << *e;
 	}
 }
--- kursova/HonestBroker.cpp
+++ kursova/HonestBroker.cpp
@@ -10,8 +10,27 @@ HonestBroker::HonestBroker(const string& Name, const double& Percent, RealEstate
 	VIPBroker(Name, Percent, Estates) {}
 
 void HonestBroker::printFoundEstates(list<Estate*> found, list<Estate*>* vip) {
+	cout << setw(126) << setfill('-') << "" << endl;
+	cout << setw(10) << setfill(' ') << left << "|Type"
+		<< setw(25) << setfill(' ') << left << "|Address"
+		<< setw(15) << setfill(' ') << left << "|Owner"
+		<< setw(10) << setfill(' ') << left << "|Price"
+		<< setw(10) << setfill(' ') << left << "|Size"
+		<< setw(10) << setfill(' ') << left << "|Rooms"
+		<< setw(10) << setfill(' ') << left << "|Floor"
+		<< setw(15) << setfill(' ') << left << "|Yard size"
+		<< setw(20) << setfill(' ') << left << "|Comunications"
+		<< "|" << endl;
+	cout << setw(126) << setfill('-') << "" << endl;
+	
 	for (auto e : *vip) {
+		int prevPrice = e->getPrice();
+
+		e->setPrice(prevPrice + prevPrice * percent);
+
 		cout << *e;
+
+		e->setPrice(prevPrice);
 	}
 
 	for (auto e : found) {
